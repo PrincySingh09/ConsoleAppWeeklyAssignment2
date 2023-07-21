dc828d37f560ff9bccb1b9babfab191bdce0d96b
princysingh2409@gmail.com <princysingh2409@gmail.com>
2023-07-21 11:48:32 +0530

    Add project files.

---
+++ ConsoleAppWeeklyAssignment2/LargeDataCollection.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Media;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppWeeklyAssignment2
+{
+    public class LargeDataCollection : IDisposable
+    {
+        private List<string> employeeList = new List<string>();
+        int employeeCount;
+        bool disposedValue = false;
+
+        public LargeDataCollection()
+        {
+            Console.WriteLine("Enter the Number of Employee information you want to Enter: ");
+            employeeCount = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < employeeCount; i++)
+            {
+                employeeList.Add(Console.ReadLine());
+            }
+        }
+
+        public void Add(string eName)
+        {
+            employeeList.Add(eName);
+            employeeCount++;
+        }
+
+        public void Remove(string eName)
+        {
+            int index = employeeList.IndexOf(eName);
+            if (index != -1) { employeeList.Remove(eName); }
+            else
+            {
+                Console.WriteLine("Employee Name is not in List!");
+            }
+
+        }
+
+        public void Search(string eName)
+        {
+            int index = employeeList.IndexOf(Name);
+            if (index == -1)
+            {
+                Console.WriteLine("Employee is not in List!");
+            }
+            else
+            {
+                Console.WriteLine("Employee is in the list at index {0}", index);
+            }
+        }
+
+        public void DisplayAll()
+        {
+            Console.WriteLine("All employee's List as follows:");
+            foreach (var employee in employeeList)
+            {
+                Console.WriteLine(employee);
+            }
+        }
+
+        public virtual void Dispose(bool disposing)
+        {
+            if (!disposedValue)
+            {
+                if (disposing)
+                {
+                    employeeCount = 0;
+                }
+                employeeList = null;
+                disposedValue = true;
+                Console.WriteLine($"The EmployeeList has been disposed");
+            }
+        }
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+    }
+}
---
+++ ConsoleAppWeeklyAssignment2/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppWeeklyAssignment2
+{
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string choice;
+            LargeDataCollection largeData = new LargeDataCollection();
+
+            do
+            {
+                Console.WriteLine("Enter the Operations: ");
+                Console.WriteLine("1. Add Employee \n 2. Remove Employee \n 3. Search Employee \n " +
+                    " 4.Show All Employee");
+                int opt = int.Parse(Console.ReadLine());
+
+                switch (opt)
+                {
+                    case 1:
+                        {
+                            Console.WriteLine("Enter the name of employee want to Add: ");
+                            string name = Console.ReadLine();
+
+                            largeData.Add(name);
+                            break;
+                        }
+                    case 2:
+                        {
+                            Console.WriteLine("Enter the name of employee want to Remove: ");
+                            string name = Console.ReadLine();
+
+                            largeData.Remove(name);
+                            break;
+                        }
+                    case 3:
+                        {
+                            Console.WriteLine("Enter the name of employee want to Search: ");
+                            string name = Console.ReadLine();
+
+                            largeData.Search(name);
+                            break;
+                        }
+                    case 4:
+                        {
+                            largeData.DisplayAll();
+                            break;
+                        }
+                    default:
+                        {
+                            Console.WriteLine("Invalid Input!");
+                            break;
+                        }
+                }
+                Console.WriteLine("Do you want to go again (Press y):");
+                choice = Console.ReadLine().ToLower();
+            } while (choice == "y");
+
+            Console.WriteLine("Data has been Disposed");
+            largeData.Dispose();
+
+            Console.ReadKey();
+
+        }
+    }
+}
